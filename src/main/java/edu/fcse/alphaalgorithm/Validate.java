5b4c750690bb093ae27d18228e7d13445a7f849d
Blagoj Atanasovski <blagoj.atanasovski@gmail.com>
2015-12-08 11:04:00 +0100

    gradle build setup, minor code renaming and organizing

--- src/main/java/edu/fcse/alphaalgorithm/Validate.java
+++ src/main/java/edu/fcse/alphaalgorithm/Validate.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-import edu.fcse.alphaalgorithm.tools.Footprint;
 import edu.fcse.alphaalgorithm.tools.Place;
 import edu.fcse.alphaalgorithm.tools.Trace;
 
@@ -15,107 +14,107 @@ public class Validate {
 	private static int[] rangeMapper = new int[21];
 	private static final int TOTAL_CASES = 1391;
 	private static final int FOLDS = 5;
-	private static int[] ammountMapper = new int[121];
+	private static int[] amountMapper = new int[121];
 
 	private static void createLog() {
 		int i = 0;
 		logMap = new HashMap<>();
 		logMap.put(0, new Trace(new String[] { "a", "c", "d", "e", "h" }));
 		rangeMapper[i] = 455;
-		ammountMapper[i] = 455;
+		amountMapper[i] = 455;
 		i++;
 		logMap.put(1, new Trace(new String[] { "a", "b", "d", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 191;
-		ammountMapper[i] = 191;
+		amountMapper[i] = 191;
 		i++;
 		logMap.put(2, new Trace(new String[] { "a", "d", "c", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 177;
-		ammountMapper[i] = 177;
+		amountMapper[i] = 177;
 		i++;
 		logMap.put(3, new Trace(new String[] { "a", "b", "d", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 144;
-		ammountMapper[i] = 144;
+		amountMapper[i] = 144;
 		i++;
 		logMap.put(4, new Trace(new String[] { "a", "c", "d", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 111;
-		ammountMapper[i] = 111;
+		amountMapper[i] = 111;
 		i++;
 		logMap.put(5, new Trace(new String[] { "a", "d", "c", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 82;
-		ammountMapper[i] = 82;
+		amountMapper[i] = 82;
 		i++;
 		logMap.put(6, new Trace(new String[] { "a", "d", "b", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 56;
-		ammountMapper[i] = 56;
+		amountMapper[i] = 56;
 		i++;
 		logMap.put(7, new Trace(new String[] { "a", "c", "d", "e", "f", "d",
 				"b", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 47;
-		ammountMapper[i] = 47;
+		amountMapper[i] = 47;
 		i++;
 		logMap.put(8, new Trace(new String[] { "a", "d", "b", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 38;
-		ammountMapper[i] = 38;
+		amountMapper[i] = 38;
 		i++;
 		logMap.put(9, new Trace(new String[] { "a", "c", "d", "e", "f", "b",
 				"d", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 33;
-		ammountMapper[i] = 33;
+		amountMapper[i] = 33;
 		i++;
 		logMap.put(10, new Trace(new String[] { "a", "c", "d", "e", "f", "b",
 				"d", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 14;
-		ammountMapper[i] = 14;
+		amountMapper[i] = 14;
 		i++;
 		logMap.put(11, new Trace(new String[] { "a", "c", "d", "e", "f", "d",
 				"b", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 11;
-		ammountMapper[i] = 11;
+		amountMapper[i] = 11;
 		i++;
 		logMap.put(12, new Trace(new String[] { "a", "d", "c", "e", "f", "c",
 				"d", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 9;
-		ammountMapper[i] = 9;
+		amountMapper[i] = 9;
 		i++;
 		logMap.put(13, new Trace(new String[] { "a", "d", "c", "e", "f", "d",
 				"b", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 8;
-		ammountMapper[i] = 8;
+		amountMapper[i] = 8;
 		i++;
 		logMap.put(14, new Trace(new String[] { "a", "d", "c", "e", "f", "b",
 				"d", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 5;
-		ammountMapper[i] = 5;
+		amountMapper[i] = 5;
 		i++;
 		logMap.put(15, new Trace(new String[] { "a", "c", "d", "e", "f", "b",
 				"d", "e", "f", "d", "b", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 3;
-		ammountMapper[i] = 3;
+		amountMapper[i] = 3;
 		i++;
 		logMap.put(16, new Trace(new String[] { "a", "d", "c", "e", "f", "d",
 				"b", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 2;
-		ammountMapper[i] = 2;
+		amountMapper[i] = 2;
 		i++;
 		logMap.put(17, new Trace(new String[] { "a", "d", "c", "e", "f", "b",
 				"d", "e", "f", "b", "d", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 2;
-		ammountMapper[i] = 2;
+		amountMapper[i] = 2;
 		i++;
 		logMap.put(18, new Trace(new String[] { "a", "d", "c", "e", "f", "d",
 				"b", "e", "f", "b", "d", "e", "h" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 1;
-		ammountMapper[i] = 1;
+		amountMapper[i] = 1;
 		i++;
 		logMap.put(19, new Trace(new String[] { "a", "d", "b", "e", "f", "b",
 				"d", "e", "f", "d", "b", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 1;
-		ammountMapper[i] = 1;
+		amountMapper[i] = 1;
 		i++;
 		logMap.put(20, new Trace(new String[] { "a", "d", "c", "e", "f", "d",
 				"b", "e", "f", "c", "d", "e", "f", "d", "b", "e", "g" }));
 		rangeMapper[i] = rangeMapper[i - 1] + 1;
-		ammountMapper[i] = 1;
+		amountMapper[i] = 1;
 		i++;
 	}
 
@@ -149,7 +148,7 @@ public class Validate {
 		Map<Trace,Integer> tmpMapa=new HashMap<>();
 		for (Integer key : logMap.keySet()) {
 			validationSet.add(logMap.get(key));
-			tmpMapa.put(logMap.get(key),ammountMapper[key]);
+			tmpMapa.put(logMap.get(key), amountMapper[key]);
 		}
 		Set<Trace> constructionSet = new HashSet<>();
 		for (int percent = 80; percent <= 95; percent += 5f) {
